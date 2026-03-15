ae3b8381355318b670228b700d72e3422f91b601
khadijarahmannupur-ctrl <khadijarahmannupur@gmail.com>
2026-03-15 14:11:05 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Countries from './components/Countirs/Countries'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+          <Countries></Countries>
+      </section>
+    </>
+  )
+}
+
+export default App
